9ddbc0c6332766d82e7e6245e398bbf2b362f899
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-12-09 18:48:09 -0500

    android app

---
+++ android_app/SP/app/src/main/java/pin3da/sp/PostBG.java
@@ -0,0 +1,50 @@
+package pin3da.sp;
+
+import android.os.AsyncTask;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+/**
+ * Created by pin3da on 11/21/15.
+ */
+public class PostBG  extends AsyncTask<String, Integer, String> {
+    protected String doInBackground(String... data)  {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        String url = data[0];
+        String json = data[1];
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+        // setProgressPercent(progress[0]);
+    }
+
+    protected void onPostExecute(String result) {
+        // showDialog("Downloaded " + result + " bytes");
+    }
+
+}
